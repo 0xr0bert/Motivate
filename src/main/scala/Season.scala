c7712612500b6048d383f03c5b554406983819a1
Robert Greener <robert.greener@kcl.ac.uk>
2018-06-11 11:54:55 +0100

    Add real values to Season based off Met Office Data

--- src/main/scala/Season.scala
+++ src/main/scala/Season.scala
@@ -2,20 +2,23 @@ sealed trait Season extends Serializable with Product {
   val percentageBadWeather: Float
 }
 
-// TODO: Change these values to something backed up with data
+/*
+ Based of Met Office data for Northolt https://www.metoffice.gov.uk/public/weather/climate/gcptq81bc#?tab=climateTables
+ Days in which there is over 1mm of rainfall
+ */
 
 case object Winter extends Season {
-  override val percentageBadWeather: Float = 0.7f
+  override val percentageBadWeather: Float = 0.3455f
 }
 
 case object Spring extends Season {
-  override val percentageBadWeather: Float = 0.5f
+  override val percentageBadWeather: Float = 0.3098f
 }
 
 case object Summer extends Season {
-  override val percentageBadWeather: Float = 0.2f
+  override val percentageBadWeather: Float = 0.2696f
 }
 
 case object Autumn extends Season {
-  override val percentageBadWeather: Float = 0.5f
+  override val percentageBadWeather: Float = 0.3275f
 }
