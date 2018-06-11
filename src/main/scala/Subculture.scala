c6e20e277fa1cc2c7fca30b716db27b36e839235
Robert Greener <robert.greener@kcl.ac.uk>
2018-06-11 09:31:22 +0100

    Add a feasibility score to Subculture

--- src/main/scala/Subculture.scala
+++ src/main/scala/Subculture.scala
@@ -1,18 +1,31 @@
 sealed trait Subculture extends Serializable with Product {
   val preferences: Map[TransportMode, Float]
+  val feasibility: Map[JourneyType, Map[TransportMode, Float]]
 }
 
 case object Pedestrian extends Subculture {
   override val preferences: Map[TransportMode, Float] =
     Map (Car -> 0.0f, PublicTransport -> 0.9f, Cycle -> 0.5f, Walk -> 0.9f) // What values?
+  override val feasibility: Map[JourneyType, Map[TransportMode, Float]] =
+    Map (LocalCommute -> Map (),
+      CityCommute -> Map(),
+      DistantCommute -> Map())
 }
 
 case object Cycling extends Subculture {
   override val preferences: Map[TransportMode, Float] =
     Map (Car -> 0.0f, PublicTransport -> 0.9f, Cycle -> 0.5f, Walk -> 0.9f) // What values?
+  override val feasibility: Map[JourneyType, Map[TransportMode, Float]] =
+    Map (LocalCommute -> Map (),
+      CityCommute -> Map(),
+      DistantCommute -> Map())
 }
 
 case object Driving extends Subculture {
   override val preferences: Map[TransportMode, Float] =
     Map (Car -> 0.0f, PublicTransport -> 0.9f, Cycle -> 0.5f, Walk -> 0.9f) // What values?
+  override val feasibility: Map[JourneyType, Map[TransportMode, Float]] =
+    Map (LocalCommute -> Map (),
+      CityCommute -> Map(),
+      DistantCommute -> Map())
 }
