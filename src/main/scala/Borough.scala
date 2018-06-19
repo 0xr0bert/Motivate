aaaaab761223cfb42a6969eafb0f80a807000da8
Robert Greener <robert.greener@kcl.ac.uk>
2018-06-19 15:14:52 +0100

    Add some more neighbourhoods and increase the size of the simulation
    Also added some output

--- src/main/scala/Borough.scala
+++ src/main/scala/Borough.scala
@@ -1,12 +1,12 @@
 import java.io.{File, PrintWriter}
 import java.time.Instant
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scalaz._
 import Scalaz._
 import org.apache.commons.math3.util.Pair
-import org.apache.commons.math3.distribution.EnumeratedDistribution
+import org.apache.commons.math3.distribution.{EnumeratedDistribution, NormalDistribution}
 
 class Borough (val id: Int,
                val totalYears: Int,
@@ -40,6 +40,8 @@ class Borough (val id: Int,
   def run(): Unit = {
     // Used for monitoring running-time
     val t0 = Instant.now().getEpochSecond
+
+    // Create the agents
     setUp()
     println(s"[$id] Agents created")
 
@@ -55,7 +57,10 @@ class Borough (val id: Int,
     val firstStats = countStats()
     writer.println(s"0,${firstStats._1},${firstStats._2},${firstStats._3},${firstStats._4},${if (weather == Bad) 1 else 0}")
 
+    // The simulation starts running here
     for(i <- 1 to totalYears * 365) {
+
+      // Only consider weekdays
       if (weekday(i)) {
         println(s"[$id] Day: $i")
         // Get the current season, and work out the weather for the season
@@ -78,6 +83,8 @@ class Borough (val id: Int,
 
     // Close the file
     writer.close()
+
+    // Report the running time for this thread
     val t1 = Instant.now().getEpochSecond
     println(s"[$id] Elapsed time ${t1 - t0}s")
   }
@@ -86,8 +93,16 @@ class Borough (val id: Int,
     * Generates the agents, allocating them to neighbourhoods, social networks, subcultures
     */
   def setUp(): Unit = {
+    // Create a mutable set of agents
     var agents: mutable.HashSet[Agent] = mutable.HashSet()
+
+    // Create the agents
     for (i <- 1 to numberOfPeople) {
+      /*
+       * perceivedEffort should be between 0 and 1, and is bound as such.
+       *
+       * Active modes have a higher s.d. as people's perception of the effort will vary much more than say driving.
+       */
       val perceivedEffort: Map[JourneyType, Map[TransportMode, Float]] = Map(
         LocalCommute -> Map(
           Walk -> bound(0, 1, randomNormal(0.2, 0.3)).toFloat,
@@ -108,20 +123,29 @@ class Borough (val id: Int,
           Car -> bound(0, 1, randomNormal(0.2, 0.1)).toFloat
         )
       )
-
+      // Choose the subculture, neighbourhood and journeyType
+      // TODO: Make neighbourhoods reflect cultures
       val subculture = chooseSubculture()
       val neighbourhood = chooseNeighbourhood()
       val journeyType = chooseJourneyType()
+
+      // Assign random floats from a U[0,1] distribution to weatherSensitivity, autonomy, and consistency
       val weatherSensitivity = scala.util.Random.nextFloat()
       val autonomy = scala.util.Random.nextFloat()
       val consistency = scala.util.Random.nextFloat()
+
+      // Suggestibility is from a N[1,0.25] distribution
       val suggestibility = randomNormal(1.0f, 0.25f).toFloat
+
+
+      // The currentMode, norm and habit are initially all equal
       val currentMode = chooseInitialNormAndHabit(
         subculture, subcultureConnectivity, suggestibility, journeyType, perceivedEffort
       )
       val norm = currentMode
       val habit = currentMode
 
+      // Create the agent and add it to the HashSet
       agents.add(new Agent(
         subculture = subculture,
         neighbourhood = neighbourhood,
@@ -140,10 +164,13 @@ class Borough (val id: Int,
       ))
       println(s"[$id] Agent $i generated")
     }
+    // Create the social network
     linkAgents(agents, numberOfSocialNetworkLinks, _.socialNetwork)
 
+    // Create a 'social network' for the neighbourhood
     val neighbourhoodsToAgents: Map[Neighbourhood, mutable.HashSet[Agent]] = agents.groupBy(_.neighbourhood)
     for ((_, localAgents) <- neighbourhoodsToAgents) linkAgents(localAgents, numberOfNeighbourLinks, _.neighbours)
+
     residents = agents.toSet
   }
 
@@ -160,10 +187,14 @@ class Borough (val id: Int,
 
   def chooseNeighbourhood(): Neighbourhood = {
     val x = scala.util.Random.nextFloat()
-    if (x <= 1) {
+    if (x <= 0.25) {
       NeighbourhoodOne
+    } else if (x <= 0.5){
+      NeighbourhoodTwo
+    } else if (x <= 0.75) {
+      NeighbourhoodThree
     } else {
-      NeighbourhoodOne
+      NeighbourhoodFour
     }
   }
 
@@ -190,8 +221,8 @@ class Borough (val id: Int,
                                ): TransportMode = {
     val subcultureWeight = subcultureConnectivity * suggestibility
     val subcultureDesirabilityWeighted: Map[TransportMode, Float] = subculture.desirability.mapValues(x => x * subcultureWeight)
-    val effortForJourneyInversed: Map[TransportMode, Float] = perceivedEffort(commuteLength).mapValues(x => 1.0f - x)
-    subcultureDesirabilityWeighted.unionWith(effortForJourneyInversed)(_ * _).maxBy(_._2)._1
+    val effortForJourneyInverted: Map[TransportMode, Float] = perceivedEffort(commuteLength).mapValues(x => 1.0f - x)
+    subcultureDesirabilityWeighted.unionWith(effortForJourneyInverted)(_ * _).maxBy(_._2)._1
   }
 
 
@@ -205,32 +236,30 @@ class Borough (val id: Int,
     * @param network The network to link
     */
   def linkAgents(agents: mutable.HashSet[Agent], n: Int, network: Agent => mutable.Set[Agent]): Unit = {
-    var unlinkedAgents = agents.clone()
     var linkedAgents: mutable.HashSet[Agent] = mutable.HashSet()
 
-    while (unlinkedAgents.nonEmpty) {
-      val r = scala.util.Random.nextInt(unlinkedAgents.size)
-      val newAgent = unlinkedAgents.iterator.drop(r).next()
-      unlinkedAgents.remove(newAgent)
-
+    for (agent <- agents) {
       if (linkedAgents.size < n) {
-        for (agent <- linkedAgents) {
-          network(newAgent).add(agent)
-          network(agent).add(newAgent)
+        for (linkedAgent <- linkedAgents) {
+          network(agent).add(linkedAgent)
+          network(linkedAgent).add(agent)
         }
       } else {
         val totalDegree = linkedAgents.map(a => network(a).size).sum
         val probabilities: java.util.List[Pair[Agent, java.lang.Double]] = linkedAgents
           .map(a => new Pair[Agent, java.lang.Double](a, java.lang.Double.valueOf(network(a).size / totalDegree)))
-          .toList.asJava
+          .toList
+          .asJava
+
         val distribution = new EnumeratedDistribution[Agent](probabilities)
 
         val friends = distribution.sample(n, new Array[Agent](n))
 
         for (friend <- friends) {
-          network(friend).add(newAgent)
-          network(newAgent).add(friend)
+          network(friend).add(agent)
+          network(agent).add(friend)
         }
+
       }
     }
   }
@@ -248,12 +277,12 @@ class Borough (val id: Int,
   }
 
   /**
-    * Returns a random nubmer from a normal distribution
+    * Returns a random number from a normal distribution
     * @param mean the mean of the normal distribution
     * @param sd the standard deviation of the normal distribution
     * @return a random number from the distribution
     */
-  def randomNormal(mean: Double, sd: Double): Double = (scala.util.Random.nextGaussian() * sd) + 1
+  def randomNormal(mean: Double, sd: Double): Double = new NormalDistribution(mean, sd).sample()
 
   /**
     * Bounds a number above or below
--- src/main/scala/Neighbourhood.scala
+++ src/main/scala/Neighbourhood.scala
@@ -5,3 +5,15 @@ sealed abstract class Neighbourhood (val supportiveness: Map[TransportMode, Floa
 case object NeighbourhoodOne extends Neighbourhood(
   Map(Car -> 0.9f, Cycle -> 0.7f, Walk -> 0.8f, PublicTransport -> 0.9f)
 )
+
+case object NeighbourhoodTwo extends Neighbourhood(
+  Map(Car -> 0.5f, Cycle -> 0.7f, Walk -> 0.8f, PublicTransport -> 1.0f)
+)
+
+case object NeighbourhoodThree extends Neighbourhood(
+  Map(Car -> 0.9f, Cycle -> 0.2f, Walk -> 0.6f, PublicTransport -> 0.5f)
+)
+
+case object NeighbourhoodFour extends Neighbourhood(
+  Map(Car -> 0.2f, Cycle -> 0.9f, Walk -> 0.9f, PublicTransport -> 0.9f)
+)
