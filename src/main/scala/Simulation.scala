4b1c2a439693f1aad3197a166f1217b37caf298c
Robert Greener <robert.greener@kcl.ac.uk>
2018-06-26 11:15:46 +0100

    Add an intervention

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -18,6 +18,12 @@ object Simulation {
       new Neighbourhood(Map(Car -> 0.5f, Cycle -> 0.7f, Walk -> 0.8f, PublicTransport -> 1.0f)),
       new Neighbourhood(Map(Car -> 0.9f, Cycle -> 0.2f, Walk -> 0.6f, PublicTransport -> 0.5f)),
       new Neighbourhood(Map(Car -> 0.2f, Cycle -> 0.9f, Walk -> 0.9f, PublicTransport -> 0.9f))
+    )),
+    "post intervention" -> new Scenario(Set(
+      new Neighbourhood(Map(Car -> 0.9f, Cycle -> 0.7f, Walk -> 0.8f, PublicTransport -> 0.9f)),
+      new Neighbourhood(Map(Car -> 0.5f, Cycle -> 0.7f, Walk -> 0.8f, PublicTransport -> 1.0f)),
+      new Neighbourhood(Map(Car -> 0.7f, Cycle -> 0.8f, Walk -> 0.6f, PublicTransport -> 0.5f)), // this line is changed
+      new Neighbourhood(Map(Car -> 0.2f, Cycle -> 0.9f, Walk -> 0.9f, PublicTransport -> 0.9f))
     ))
   )
 
