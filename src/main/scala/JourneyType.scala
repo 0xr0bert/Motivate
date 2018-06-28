5438c6f7ef9ff3108d034523df9b4fb07ab8d4a1
Robert Greener <robert.greener@kcl.ac.uk>
2018-06-28 10:28:17 +0100

    Add cost to JourneyType

--- src/main/scala/JourneyType.scala
+++ src/main/scala/JourneyType.scala
@@ -1,4 +1,19 @@
-sealed trait JourneyType extends Serializable with Product
-case object LocalCommute extends JourneyType
-case object CityCommute extends JourneyType
-case object DistantCommute extends JourneyType
+sealed abstract class JourneyType(val cost: Map[TransportMode, Float])
+case object LocalCommute extends JourneyType(Map(
+  Car -> 0.1f,
+  PublicTransport -> 0.1f,
+  Walk -> 0.2f,
+  Cycle -> 0.4f
+))
+case object CityCommute extends JourneyType(Map(
+  Car -> 0.1f,
+  PublicTransport -> 0.1f,
+  Walk -> 0.9f,
+  Cycle -> 0.6f
+))
+case object DistantCommute extends JourneyType (Map(
+  Car -> 0.1f,
+  PublicTransport -> 0.1f,
+  Walk -> 1.1f, // This should make it impossible
+  Cycle -> 1.1f // This should make it impossible
+))
